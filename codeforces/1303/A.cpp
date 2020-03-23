8d8b725c1bcfca5f8ae616519f42f01c91ce5ec1
Aniket Shukla <aniketshukla3553@gmail.com>
2020-03-23 19:50:00 +0530

    Add solution for problem `A - Erasing Zeroes` Link: https://codeforces.com/contest/1303/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1303/submission/74045522

---
+++ codeforces/1303/A.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cctype>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <list>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <string>
+#include <cmath>
+#include "bits/stdc++.h"
+using namespace std;
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
+#define mod 1e9+7
+#define FOR(a,c)   for ( int (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( int (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  int (a)=(b); (a)>=(c); (a)--)
+#define INF 1000000000000000003
+typedef long long int ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+/*
+
+typedef pair<int,int>   PI;
+typedef vector<int> VI;
+typedef long long int LL;
+*/
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+/*continue
+cin
+cout
+while
+if else
+string*/
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t,n,k,m;
+	string a;
+	cin>>t;
+	while(t--)
+	{
+		cin>>a;
+		n=a.size();
+		vi p;
+		FOR(i,n)
+		{
+			if(a[i]=='1')
+				p.PB(i);
+		}
+		k=0;
+		if(p.size()==0)
+		{	cout<<"0\n";
+			continue;
+		}
+		FOR(i,p.size()-1)
+		{
+			k+=p[i+1]-p[i]-1;
+		}
+		cout<<k<<"\n";
+		
+	}
+}
