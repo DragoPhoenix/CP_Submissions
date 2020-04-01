ace23a3a81e5cfa7953cf479fc47a9571e83f726
Aniket Shukla <aniketshukla3553@gmail.com>
2020-04-01 21:29:00 +0530

    Add solution for problem `B - Bogosort` Link: https://codeforces.com/contest/1312/problem/B Tags: constructive algorithms, sortings, *1000 Ref: https://codeforces.com/contest/1312/submission/75148462

---
+++ codeforces/1312/B.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cctype>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <list>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <string>
+#include <cmath>
+#include "bits/stdc++.h"
+using namespace std;
+
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( int (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( int (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  int (a)=(b); (a)>=(c); (a)--)
+
+#define INF 1000000000000000003
+
+#define abs llabs
+#define BOOST ios::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
+
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
+#define run if(1)cout<<__LINE__<<endl;  
+
+#define pr3(a,b,c) cout<<"var1="<<a<<"\n"<<"var2="<<b<<"\n"<<"var3="<<c<<"\n";
+#define pr2(a,b) cout<<"var1="<<a<<"\n"<<"var2="<<b<<"\n";
+#define pr1(a) cout<<"var1="<<a<<"\n";
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+
+void solve()
+{
+	ll n,m,d,k,z,c=0;
+	cin>>n;
+	int a[n+1],s[n+1];
+	a[0]=s[0]=0;
+	FORL(i,1,n+1)
+	{
+		cin>>a[i];
+		//s[i]=a[i]-i;
+	}
+	sort(a,a+n+1);
+	FORR(i,n,1)
+	cout<<a[i]<<" ";
+	cout<<"\n";
+
+	
+}
+
+int main() 
+{
+	BOOST
+	ll T=1,t=0;
+	cin>>T;
+	while(t++<T)
+	{
+		//cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        //cout<<'\n';
+	}
+	cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
