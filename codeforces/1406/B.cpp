afe037b0f0473852db595a9aa51e4bb02a3d2f6e
Aniket Shukla <aniketshukla3553@gmail.com>
2020-12-14 19:37:00 +0530

    Add solution for problem `B - Maximum Product` Link: https://codeforces.com/contest/1406/problem/B Tags: brute force, dp, greedy, implementation, sortings, *1200 Ref: https://codeforces.com/contest/1406/submission/101176758

---
+++ codeforces/1406/B.cpp
@@ -0,0 +1,198 @@
+#pragma GCC optimize("Ofast")
+#define _USE_MATH_DEFINES
+#include "bits/stdc++.h"
+using namespace std;
+
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
+
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
+#define run if(1)cout<<__LINE__<<endl;  
+
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int i = 0; i<N;i++) cerr<<A[i]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int i=0;i<N;i++){cerr<<"[";for(int j=0;j<M;++j) cerr<<A[i][j]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int i = 0; i< (v).size(); i++) cerr<<v[i]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int i=0;i<v.size();i++){cerr<<"[";for(int j=0;j<(v[0].size());++j) cerr<<v[i][j]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin(); itr!=m.end();itr++) cerr<<*itr<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin();itr!=m.end(); itr++) cerr<<"("<<itr->first<<","<<itr->second<<") ";cerr<<"]\n";
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+void solve()
+{
+	ll n,m,t=0,k=0,l,x=0,y=0,z=0,p,a1,a2,a3,a4,a5,ex,che,var=1,yd,f=INF,templ,tempr; 
+	cin>>n;
+	// pr1(n)
+	vi a(n);
+	// pr1("Ok")
+	vector <pi> b(n);
+	// pr1("Ok")
+	FOR(i,n)
+	{
+		cin>>a[i];
+		k=1;
+		if(a[i]<0)k=-1;
+		b[i]={abs(a[i]),k};
+	}
+	sort(b.begin(), b.end());
+	k=1;
+	FORR(i,n-1,n-5)
+	{
+		k*=b[i].F;
+		k*=b[i].S;
+		if(b[i].S==-1)
+		{
+			x++;
+			a1=b[i].F;
+		}
+		else
+			a2=b[i].F;
+	}
+	z=k;
+	// pr1(z)
+	if(z==0)
+	{
+		cout<<z<<"\n";
+		return;
+	}
+	if(x==1||x==3)
+	{
+		if(n>5)
+		{
+			// debugv(a)
+			// pr1(k)
+			// y=b[n-6].F;
+			// z=b[n-6].S;
+			// if(z<0)
+			// {
+			// 	k/=a2;
+			// 	k*=y;
+			// }
+			// else
+			// {
+			// 	k/=a1;
+			// 	k*=y;
+			// }
+			// if(k<0)
+			// 	k*=-1;
+			z=k;
+			a3=a4=50000;
+			FORR(i,n-6,0)
+			{
+				if(b[i].S==1&&a3==50000)
+					a3=b[i].F;
+				if(b[i].S==-1&&a4==50000)
+					a4=b[i].F;
+			}
+			// pr3(a3,a4,a1)/
+			if(a3!=50000)
+			{
+				y=k;
+				k/=(a1*-1);
+				k*=a3;
+				z=max(z,k);
+				k=y;
+			}
+			// pr3(z,a4,a2)
+			if(a4!=50000)
+			{
+				y=k;
+				k/=a2;
+				k*=a4*-1;
+				z=max(z,k);
+				k=y;
+			}
+		}
+	}
+	else if(x==5)
+	{
+		if(n>5)
+		{
+			z=k;
+			// pr1(z)
+			a3=50000;
+			FORR(i,n-6,0)
+			{
+				// pr2(b[i].F,b[i].S)
+				if(b[i].S==1&&a3==50000)
+					a3=b[i].F;
+			}
+			// pr2(a1,a3)
+			if(a3!=50000)
+			{
+				k/=(a1*-1);
+				k*=a3;
+				z=max(z,k);
+			}
+			else
+			{
+				z=1;
+				FOR(i,5)z*=b[i].F*b[i].S;
+			}
+			// if(a4!=50000)
+			// {
+			// 	k/=a2;
+			// 	k*=a4*-1;
+			// 	z=max(z,k);
+			// }
+		}
+	}
+	cout<<z<<"\n";
+
+}	
+
+
+int main() 
+{
+	by_ANIKET
+	ll T=1,t=0;
+	cin>>T;
+	while(t++<T)
+	{
+		//cout<<"Case "<<t<<":"<<' ';
+        solve();
+        //cout<<'\n';
+	}
+	cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓     
