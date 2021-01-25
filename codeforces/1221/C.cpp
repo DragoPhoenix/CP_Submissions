1350b4b8feccf64655091cdb0a5a7fcaaa41bfb7
Aniket Shukla <aniketshukla3553@gmail.com>
2021-01-25 14:37:00 +0530

    Add solution for problem `C - Perfect Team` Link: https://codeforces.com/contest/1221/problem/C Tags: binary search, math, *1200 Ref: https://codeforces.com/contest/1221/submission/105282272

---
+++ codeforces/1221/C.cpp
@@ -0,0 +1,144 @@
+#pragma GCC optimize("Ofast")
+#define _USE_MATH_DEFINES
+#include "bits/stdc++.h"
+using namespace std;
+
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
+
+#define MOD 1000000007
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
+
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
+#define run if(1)cout<<__LINE__<<endl;  
+
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int i = 0; i<N;i++) cerr<<A[i]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int i=0;i<N;i++){cerr<<"[";for(int j=0;j<M;++j) cerr<<A[i][j]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int i = 0; i< (v).size(); i++) cerr<<v[i]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int i=0;i<v.size();i++){cerr<<"[";for(int j=0;j<(v[0].size());++j) cerr<<v[i][j]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin(); itr!=m.end();itr++) cerr<<*itr<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin();itr!=m.end(); itr++) cerr<<"("<<itr->first<<","<<itr->second<<") ";cerr<<"]\n";
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+void solve()
+{
+	ll n,m,z=INF,x=0,y=0,a1,a2,a3,a4,a5,ex,che,var,k=0;
+	cin>>n>>m>>x;
+	//a1=min(n,m);//ans<=a1
+	if(n>m)swap(n,m);
+	if(x>=n)
+	{
+		cout<<n<<"\n";
+		return;
+	}
+	//x<n
+	z=x;
+	n-=x;
+	m-=x;//1,2,3
+
+	// k=m-n;
+	// z+=k;
+	// n-=k;
+	// m-=2*k;
+	// assert(n==m);//1,2,2
+
+	// if(n<0)
+	// {
+	// 	z+=n;
+	// 	cout<<z<<"\n";
+	// 	return;
+	// }
+	// x=n/3;
+	// z+=2*x;
+	// n-=3*x;
+	// m-=3*x;//1,2,2 && 1,1,2
+
+	// while(n+m>2)//left
+	// {
+	// 	if(n>m)swap(n,m);
+	// 	k=m-n;
+	// 	z+=k;
+	// 	if(!k)
+	// 	{
+	// 		n--;
+	// 		m-=2;
+	// 		if(n>=0&&m>=0)
+	// 		z++;
+	// 	}
+	// 	else
+	// 	{
+	// 		n-=k;
+	// 		m-=2*k;
+	// 	}
+	// }
+	// cout<<z<<"\n";
+
+	//n,m,0
+	//no of teams b/w 1 and (n+m)/3
+	y=0;
+	int l=0,r=(n+m)/3;
+	while(l<=r)
+	{
+		int mid=l+(r-l)/2;
+		if(n+m-3*mid>=0&&n>=mid&&m>=mid)
+		{
+			y=mid;
+			l=mid+1;
+		}
+		else
+			r=mid-1;
+	}
+	cout<<z+y<<"\n";
+}
+
+int main() 
+{
+	by_ANIKET
+	ll T=1,t=0;
+	cin>>T;
+	while(t++<T)
+	{
+		// cout<<"Case "<<t<<":"<<' ';
+        solve();
+        //cout<<'\n';
+	}
+	cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓     
