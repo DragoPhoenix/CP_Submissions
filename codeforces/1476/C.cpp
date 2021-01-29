63899d26fdeb21715b278e2463efa577750a199b
Aniket Shukla <aniketshukla3553@gmail.com>
2021-01-29 22:26:00 +0530

    Add solution for problem `C - Longest Simple Cycle` Link: https://codeforces.com/contest/1476/problem/C Tags: dp, graphs, greedy, *1600 Ref: https://codeforces.com/contest/1476/submission/105942818

--- codeforces/1476/C.cpp
+++ codeforces/1476/C.cpp
@@ -1,36 +1,46 @@
 //Hey stalker :)
+#pragma GCC optimize("Ofast")
+#define _USE_MATH_DEFINES
 #include "bits/stdc++.h"
 using namespace std;
+
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
 
 #define MOD 1000000007
 
 #define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
 #define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
 #define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define all(v)                      (v).begin(),(v).end()
+#define Sort(v)                     sort(all((v)))
+#define rSort(v)                    sort((v).rbegin(),(v).rend())
+#define tr(container, it) for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b)  (x/gcd(x,y))*y
 #define INF 9223372036854775807
 //18446744073709551615
 #define abs llabs
-#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL);
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
 
 #define no cout<<"No\n";
 #define yes cout<<"Yes\n";
 
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
 #define ppc               __builtin_popcount
 #define ppcll             __builtin_popcountll
 
-#define pr4(a,b,c,d) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\t"<<#d<<"="<<d<<"\n";
 #define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
 #define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
 #define pr1(a) cerr<<#a<<"="<<a<<"\n";
 #define debug(x)       cerr<< #x <<" : "<< x << endl;
-#define debuga(A,N)    cerr<< #A <<" : [";for(int india = 0; india<N;india++) cerr<<A[india]<<" "; cerr<<"]\n";
-#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int india=0;india<N;india++){cerr<<"[";for(int sachin=0;sachin<M;++sachin) cerr<<A[india][sachin]<<" ";cerr<<"]\n";}
+#define debuga(A,N)    cerr<< #A <<" : [";for(int i = 0; i<N;i++) cerr<<A[i]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int i=0;i<N;i++){cerr<<"[";for(int j=0;j<M;++j) cerr<<A[i][j]<<" ";cerr<<"]\n";}
 #define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
-#define debugv(v)      cerr<< #v <<" : "<<"[";for(int india = 0; india< (v).size(); india++) cerr<<v[india]<<" "; cerr<<"]\n";
-#define debugv2(v)     cerr<< #v << " : \n"; for(int india=0;india<v.size();india++){cerr<<"[";for(int sachin=0;sachin<(v[india].size());++sachin) cerr<<v[india][sachin]<<" ";cerr<<"]\n";}
-#define debugs(m)      cerr<< #m <<" : [ "; for(auto india = m.begin(); india!=m.end();india++) cerr<<*india<<" "; cerr<<"]\n";
-#define debugm(m)      cerr<< #m <<" : [ "; for(auto india = m.begin();india!=m.end(); india++) cerr<<"("<<india->first<<","<<india->second<<") ";cerr<<"]\n";
-
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int i = 0; i< (v).size(); i++) cerr<<v[i]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int i=0;i<v.size();i++){cerr<<"[";for(int j=0;j<(v[0].size());++j) cerr<<v[i][j]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin(); itr!=m.end();itr++) cerr<<*itr<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin();itr!=m.end(); itr++) cerr<<"("<<itr->first<<","<<itr->second<<") ";cerr<<"]\n";
 typedef unsigned long long ull;
 typedef long long int ll;
 typedef vector<ll> vi;
@@ -48,37 +58,50 @@ const ll N=2e5+5;
 
 void solve()
 {
-    ll n, m, t = 0, k = 0, x = 0, y = 0, z = 0, a1, a2, a3, a4, a5, var = 1, f = INF;
-    string s;
+    ll n,m,t=0,k=0,x=0,y=0,z=0,a1,a2,a3,a4,a5,var=1,f=INF;
+    cin>>n;
+    vi a(n),b(n),c(n),vp1(n,0),vp(n,0);
+    FOR(i,n)cin>>c[i];
+    FOR(i,n)cin>>a[i];
+    FOR(i,n)cin>>b[i];
 
-    cin >> n;
-    vi c(n), a(n), b(n);
-
-    FOR(i, n) cin >> c[i];
-    FOR(i, n) cin >> a[i];
-    FOR(i, n) cin >> b[i];
-
-    vi v(n, 0);
-    FORL(i, 1,  n)
+    FORL(i,1,n)
     {
-        v[i] = max(v[i], c[i] - 1 + 2 + abs(a[i] - b[i]));
-
-        if(a[i] != b[i])
-        v[i] = max(v[i-1] - abs(a[i] - b[i]) + 2 + c[i] - 1, v[i]);
+        vp[i]=c[i]-1+2+abs(b[i]-a[i]); 
+        // if(vp[i-1])
+        // pr3(max3(0,vp[i-1],vp1[i-1]),(c[i-1]-1-abs(a[i]-b[i])),c[i]-1+2)
+        vp1[i]=c[i]-1+2+(max3(0,vp[i-1],vp1[i-1])-abs(a[i]-b[i]));
+        if(max3(0,vp[i-1],vp1[i-1])==0)
+            vp1[i]=0;
+        if(a[i]==b[i])
+            vp1[i]=vp[i];
+        // if((c[i-1]-1-abs(a[i]-b[i]))==0)
+        // {
+        //     vp1[i]=c[i]-1+2+(max3(0,vp[i-1],vp1[i-1]))-c[i-1]+1;
+        // }
     }
-
-    for(auto e : v)
-    z = max(z, e);
-
-    cout << z << "\n";
+    // debugv(vp)
+    // debugv(vp1)
+    z=0;
+    FOR(i,n)
+    {
+        z=max(z,vp[i]);
+        z=max(z,vp1[i]);
+    }
+    cout<<z<<"\n";
 }
 
 int main() 
 {
+    // #ifndef ONLINE_JUDGE
+    // freopen("Input.txt", "r", stdin);
+    // freopen("Output.txt", "w", stdout);
+    // freopen("Error.txt","w",stderr);
+    // #endif
     by_ANIKET
-    ll T = 1,t = 0;
-    cin >> T;
-    while(t++ < T)
+    ll T=1,t=0;
+    cin>>T;
+    while(t++<T)
     {
         // cout<<"Case #"<<t<<":"<<' ';
         solve();
@@ -96,3 +119,4 @@ int main()
 //                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
 //                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
 //                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
+    
