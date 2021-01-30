2f4e78267ac51b6412273fa3bc0898aa26d5e0f9
Aniket Shukla <aniketshukla3553@gmail.com>
2021-01-30 18:04:00 +0530

    Add solution for problem `C - Bowls and Dishes` Link: https://atcoder.jp/contests/abc190/tasks/abc190_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc190/submissions/19799601

---
+++ atcoder/abc190/C.cpp
@@ -0,0 +1,169 @@
+//Hey stalker :)
+#pragma GCC optimize("Ofast")
+#define _USE_MATH_DEFINES
+#include "bits/stdc++.h"
+using namespace std;
+
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define all(v)                      (v).begin(),(v).end()
+#define Sort(v)                     sort(all((v)))
+#define rSort(v)                    sort((v).rbegin(),(v).rend())
+#define tr(container, it) for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b)  (x/gcd(x,y))*y
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int i = 0; i<N;i++) cerr<<A[i]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int i=0;i<N;i++){cerr<<"[";for(int j=0;j<M;++j) cerr<<A[i][j]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int i = 0; i< (v).size(); i++) cerr<<v[i]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int i=0;i<v.size();i++){cerr<<"[";for(int j=0;j<(v[0].size());++j) cerr<<v[i][j]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin(); itr!=m.end();itr++) cerr<<*itr<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin();itr!=m.end(); itr++) cerr<<"("<<itr->first<<","<<itr->second<<") ";cerr<<"]\n";
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+ll N=2e5+5,M,Z=0;
+std::vector<pi> a(105); 
+std::vector<pi> c(18);
+int K;
+    
+void rec(vi &aa,int n)
+{
+    // pr1(n)
+    // debugv(aa)
+    if(!n)
+    {
+        
+        // a.PB(c[n].F);
+        vi arr1(N+2,0),arr2(N+2,0);
+        aa[0]=1;
+        // debugv(aa)
+        FOR(i,K)
+        {
+            if(aa[i]==0)
+                arr1[c[i].F]++;
+            else
+                arr1[c[i].S]++;
+        }
+        int z=0;
+        FOR(i,M)
+        {
+            if(arr1[a[i].F]&&arr1[a[i].S])
+                z++;
+        }
+        // if(z==12)
+        // {
+        //     debugv(arr1)
+        // }
+        Z=max(z,Z);
+
+        aa[0]=0;
+        // debugv(aa)
+        FOR(i,K)
+        {
+            if(aa[i]==0)
+                arr2[c[i].F]++;
+            else
+                arr2[c[i].S]++;
+        }
+        z=0;
+        FOR(i,M)
+        {
+            if(arr2[a[i].F]&&arr2[a[i].S])
+                z++;
+        }
+        // if(z==12)
+        // {
+        //     debugv(arr2)
+        // }
+        Z=max(z,Z);
+        return;
+    }
+    aa[n]=1;
+    rec(aa,n-1);
+    aa[n]=0;
+    rec(aa,n-1);
+}
+void solve()
+{
+    ll n,m,t=0,k=0,x=0,y=0,z=0,a1,a2,a3,a4,a5,var=1,f=INF;
+    cin>>N>>M;
+    vi b(N+2,0),cou(N+2,0);
+    FOR(i,M)
+    {
+        cin>>a[i].F>>a[i].S;
+        b[a[i].F]++;
+        b[a[i].S]++;
+    }
+    cin>>K;
+    FOR(i,K)
+    {
+        cin>>c[i].F>>c[i].S;
+    }
+    vi emp(18,0);
+    // pr1("ok")
+    rec(emp,K-1);
+    cout<<Z<<"\n";
+}
+
+int main() 
+{
+    // #ifndef ONLINE_JUDGE
+    // freopen("Input.txt", "r", stdin);
+    // freopen("Output.txt", "w", stdout);
+    // freopen("Error.txt","w",stderr);
+    // #endif
+    by_ANIKET
+    ll T=1,t=0;
+    // cin>>T;
+    while(t++<T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
+    
