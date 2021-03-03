4328402e80a7c3f2bcdfd8afad5ffd8c72262b6a
Aniket Shukla <aniketshukla3553@gmail.com>
2021-03-03 20:35:00 +0530

    Add solution for problem `A - Lucky Sum of Digits` Link: https://codeforces.com/contest/109/problem/A Tags: brute force, implementation, *1000 Ref: https://codeforces.com/contest/109/submission/109017806

---
+++ codeforces/109/A.cpp
@@ -0,0 +1,118 @@
+#pragma GCC optimize("Ofast")
+#define _USE_MATH_DEFINES
+#include "bits/stdc++.h"
+using namespace std;
+
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
+
+#define MOD 1000000000
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
+
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
+#define run if(1)cout<<__LINE__<<endl;  
+
+#define pr3(a,b,c) cout<<"var1="<<a<<"\n"<<"var2="<<b<<"\n"<<"var3="<<c<<"\n";
+#define pr2(a,b) cout<<"var1="<<a<<"\n"<<"var2="<<b<<"\n";
+#define pr1(a) cout<<"var1="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int i = 0; i<N;i++) cerr<<A[i]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int i=0;i<N;i++){cerr<<"[";for(int j=0;j<M;++j) cerr<<A[i][j]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int i = 0; i< (v).size(); i++) cerr<<v[i]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int i=0;i<v.size();i++){cerr<<"[";for(int j=0;j<(v[0].size());++j) cerr<<v[i][j]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin(); itr!=m.end();itr++) cerr<<*itr<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin();itr!=m.end(); itr++) cerr<<"("<<itr->first<<","<<itr->second<<") ";cerr<<"]\n";
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+void solve()
+{
+	ll n,m,k,c=0,x=0,y=0,z,L=INF,U;
+	cin>>n;
+	std::vector<pi> v;
+	FORR(i,250005,0)
+	{
+		z=i*4;
+		if(n>=z)
+		{
+			k=n-z;
+			if(k%7==0)
+			{
+				x=i;
+				y=k/7;
+				if(x+y<L)
+				{
+					L=x+y;
+					c=x;
+					U=y;
+				}
+				else if(x+y==L&&x>c)
+				{
+					c=x;
+					U=y;
+				}
+			}
+		}
+	}
+	x=c;
+	y=U;
+	z=x*4+7*y;
+	if(z==n)
+	{
+		FOR(i,x)cout<<4;
+		FOR(i,y)cout<<7;
+		cout<<"\n";
+	}
+	else
+		cout<<"-1\n";
+}
+
+int main() 
+{
+	by_ANIKET
+	ll T=1,t=0;
+	//cin>>T;
+	while(t++<T)
+	{
+		//cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        //cout<<'\n';
+	}
+	cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+/*//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
+1
+S2(S3(ES)SE4(SE)6(2(NW))SE)SSEE    */
