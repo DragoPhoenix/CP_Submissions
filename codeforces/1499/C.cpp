7215d7927c33644a1e02118be27cf9b613813b30
Aniket Shukla <aniketshukla3553@gmail.com>
2021-03-18 21:54:00 +0530

    Add solution for problem `C - Minimum Grid Path` Link: https://codeforces.com/contest/1499/problem/C Tags: brute force, data structures, greedy, math, *1500 Ref: https://codeforces.com/contest/1499/submission/110376210

---
+++ codeforces/1499/C.cpp
@@ -0,0 +1,144 @@
+//Hey stalker :)
+#pragma GCC optimize("Ofast")
+#define _USE_MATH_DEFINES
+#include "bits/stdc++.h"
+using namespace std;
+
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define all(v)                      (v).begin(),(v).end()
+#define Sort(v)                     sort(all((v)))
+#define rSort(v)                    sort((v).rbegin(),(v).rend())
+#define tr(container, it) for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b)  (x/gcd(x,y))*y
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int i = 0; i<N;i++) cerr<<A[i]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int i=0;i<N;i++){cerr<<"[";for(int j=0;j<M;++j) cerr<<A[i][j]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int i = 0; i< (v).size(); i++) cerr<<v[i]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int i=0;i<v.size();i++){cerr<<"[";for(int j=0;j<(v[0].size());++j) cerr<<v[i][j]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin(); itr!=m.end();itr++) cerr<<*itr<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin();itr!=m.end(); itr++) cerr<<"("<<itr->first<<","<<itr->second<<") ";cerr<<"]\n";
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N=1e7+5;
+
+void solve()
+{
+    ll n,m,t=0,k=0,x=0,y=0,z=0,a1,a2,a3,a4,a5,var=1,f=INF;
+    cin>>n;
+    vi a(n);
+    FOR(i,n)cin>>a[i];
+    vi odd_sum,even_sum;
+    even_sum.PB(a[0]);
+    odd_sum.PB(a[1]);
+    k=1;
+    FORL(i,2,n)
+    {
+        even_sum.PB(a[i]);
+        i++;
+    }
+    FORL(i,3,n)
+    {
+        odd_sum.PB(a[i]);
+        i++;
+    }
+    FORL(i,1,odd_sum.size())
+    {
+        odd_sum[i]+=odd_sum[i-1];
+    }
+    FORL(i,1,even_sum.size())
+        even_sum[i]+=even_sum[i-1];
+
+    // debugv(even_sum)
+    // debugv(odd_sum)
+
+    z=n*a[0]+n*a[1];
+
+    a1=a2=1;
+    a3=a4=1;
+    y=n*a[1];
+    // pr2("Bef",z)
+    ll mi1=a[0],mi2=a[1];
+    FORL(i,2,n)
+    {
+        if(i%2==0)
+        {
+            mi1=min(mi1,a[i]);
+            x=(n-a1)*mi1 + even_sum[a3]-mi1;
+            a3++;
+            a1++;
+            // pr3(x,y,z)
+            z=min(x+y,z);
+            // pr2(x+y,z)
+        }
+        else
+        {
+            mi2=min(mi2,a[i]);
+            y=(n-a2)*mi2 + odd_sum[a4]-mi2;
+            a4++;
+            a2++;
+            // pr3(x,y,z)
+            z=min(z,x+y);
+            // pr2(x+y,z)
+        }
+    }
+    cout<<z<<"\n";
+}
+
+int main() 
+{
+    by_ANIKET
+    ll T=1,t=0;
+    cin>>T;
+    while(t++<T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
