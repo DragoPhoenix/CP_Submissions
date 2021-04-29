f394702c47f5d1805dd7e9408fe17860551efbde
Aniket Shukla <aniketshukla3553@gmail.com>
2021-04-30 01:16:00 +0530

    Add solution for problem `C - Berland Regional` Link: https://codeforces.com/contest/1519/problem/C Tags: brute force, data structures, greedy, number theory, sortings, *1400 Ref: https://codeforces.com/contest/1519/submission/114630704

---
+++ codeforces/1519/C.cpp
@@ -0,0 +1,155 @@
+//Hey stalker :)
+#pragma GCC optimize("Ofast")
+#define _USE_MATH_DEFINES
+#include "bits/stdc++.h"
+using namespace std;
+
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define all(v)                      (v).begin(),(v).end()
+#define Sort(v)                     sort(all((v)))
+#define rSort(v)                    sort((v).rbegin(),(v).rend())
+#define tr(container, it) for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b)  (x/gcd(x,y))*y
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int i = 0; i<N;i++) cerr<<A[i]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int i=0;i<N;i++){cerr<<"[";for(int j=0;j<M;++j) cerr<<A[i][j]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int i = 0; i< (v).size(); i++) cerr<<v[i]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int i=0;i<v.size();i++){cerr<<"[";for(int j=0;j<(v[0].size());++j) cerr<<v[i][j]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin(); itr!=m.end();itr++) cerr<<*itr<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin();itr!=m.end(); itr++) cerr<<"("<<itr->first<<","<<itr->second<<") ";cerr<<"]\n";
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N=2e5+5;
+
+
+/*
+1,1  1,2  1,3  1,4  1,5  1,6  1,7
+2,1  2,2  2,3  2,4  2,5  2,6  2,7
+3,1  3,2  3,3  3,4  3,5  3,6  3,7
+
+6 R , 2 D
+1 + 1/2/3/
+*/
+void solve()
+{
+    ll n,m,t=0,k=0,x=0,y=0,z=0,a1,a2,a3,a4,a5,var=1,f=INF;
+    cin >> n;
+    vi u(n),s(n);
+    map <ll, vector<ll> > op;
+    FOR(i,n)
+    	cin>>u[i];
+    FOR(i,n)
+    {
+	    cin>>s[i];
+    	op[u[i]].PB(s[i]);
+    	z+=s[i];
+    }
+    vi ans(n,0);
+    vi ans2(n+2,0);
+    // vector<vector<int> > mark(n, vector<int>(n, 0));
+
+    // debugv(ans)
+    // debugv(ans2)
+    for(auto e:op)
+    {
+    	sort(e.S.begin(), e.S.end());
+    	a1 = e.S.size();
+    	FORL(i, 0, e.S.size())
+    	{
+    		if(i!=0)
+	    		e.S[i] += e.S[i-1];
+    	}
+    	if(a1 != 0)
+    		a3 = e.S[a1-1];
+    // 	debugv(ans)
+    // debugv(ans2)
+    // debugv(e.S)
+    // pr1(a3)
+    	FORL(i, 1, n)
+    	{
+    		//i+1
+    		a2 = a1 % (i + 1);
+    		// pr3(a1,(i+1),a2)
+    		if(a2 == a1)
+    		{
+    			ans2[i] += (a3 - e.S[a2-1]); // remove
+    			// ans2[n] -= ()
+    			break;
+    		}
+    		// pr2(ans[i], e.S[a2-1])
+    		if(a2!=0)
+	    		ans[i] += (a3 - e.S[a2-1]);
+    		else
+    			ans[i] +=a3;
+    	}
+    }
+    // debugv(ans)
+    // debugv(ans2)
+    FORL(i,1,n+1)
+    ans2[i] += ans2[i-1];
+    cout << z << " ";
+    FORL(i, 1, n)
+    {
+    	cout<<ans[i] + ans2[i] <<" ";
+    }
+    cout<<"\n";
+}
+
+int main() 
+{
+    by_ANIKET
+    ll T=1,t=0;
+    cin>>T;
+    while(t++<T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
