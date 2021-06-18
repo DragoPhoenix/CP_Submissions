b230c3e7e49caa45d2de34365bd9a9b128ee445e
Aniket Shukla <aniketshukla3553@gmail.com>
2021-06-18 19:55:00 +0530

    Add solution for problem `E - Elections` Link: https://codeforces.com/contest/1267/problem/E Tags: greedy, *1700 Ref: https://codeforces.com/contest/1267/submission/119803408

---
+++ codeforces/1267/E.cpp
@@ -0,0 +1,180 @@
+//Hey stalker :)
+#pragma GCC optimize("Ofast")
+#define _USE_MATH_DEFINES
+#include "bits/stdc++.h"
+using namespace std;
+
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define all(v)                      (v).begin(),(v).end()
+#define Sort(v)                     sort(all((v)))
+#define rSort(v)                    sort((v).rbegin(),(v).rend())
+#define tr(container, it) for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+// #define gcd(a,b) __gcd(a,b)
+// #define lcm(a,b)  (x/gcd(x,y))*y
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL); cout.tie(NULL);
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int i = 0; i<N;i++) cerr<<A[i]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int i=0;i<N;i++){cerr<<"[";for(int j=0;j<M;++j) cerr<<A[i][j]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int j = 0; j< (v).size(); j++) cerr<<v[j]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int i=0;i<v.size();i++){cerr<<"[";for(int j=0;j<(v[i].size());++j) cerr<<v[i][j]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin(); itr!=m.end();itr++) cerr<<*itr<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin();itr!=m.end(); itr++) cerr<<"("<<itr->first<<","<<itr->second<<") ";cerr<<"]\n";
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N=1e6+5;
+
+void solve()
+{
+    ll n, m, t=0, k=0, x=0, y=0, z=0, a1, a2, a3, a4, a5, var=1, f=INF;    
+    
+    cin >> n >> m;
+
+    vector < vi > v(m, vi(n)), vt;
+    vi sum(n, 0), sumt;
+
+    FOR(i, m)
+    {
+        FOR(j, n)
+        {
+            cin >> v[i][j];
+            sum[j] += v[i][j];
+        }
+    }
+
+    x = sum[n-1];
+    y = 1;
+    FORR(i, n - 2, 0)
+    {
+        if(sum[i] >= x)
+        {
+            y = 0;
+            break;
+        }
+    }
+    if(!y)
+    {
+        cout << "0\n";
+        return;
+    }
+
+    vt = v;
+    sumt = sum;
+    z = 105;
+    vi ans, anst;
+    FOR(i, n - 1)
+    {
+        // i-th one will beat
+        vt = v;
+        sumt = sum;
+        k = 0;
+        anst.clear();
+        while(true)
+        {
+            k++;
+            // debugv2(vt)
+            // pr2(k, i)
+            x = -MOD;
+            y = -1;
+            FOR(j, m)
+            {
+                if(vt[j][n-1] - vt[j][i] > x && vt[j][i] != -1)
+                {
+                    x = vt[j][n-1] - vt[j][i];
+                    y = j;
+                }
+            }
+            // debugv(vt[j])
+            // pr1(y)
+            // pr1(x)
+            if(y == -1)
+                break;
+            FOR(j, n)
+            {
+                sumt[j] -= vt[y][j];
+                vt[y][j] = -1;
+            }
+            anst.PB(y+1);
+            x = sumt[n-1];
+            y = 1;
+            FORR(i, n - 2, 0)
+            {
+                if(sumt[i] >= x)
+                {
+                    y = 0;
+                    break;
+                }
+            }
+            if(!y)
+            {
+                if(z == k)
+                    break;
+                z = min(z, k);
+                if(z == k)
+                    ans = anst;
+                break;
+            }
+            // debugv(sumt)
+            // debugv2(vt)
+        }
+    }
+    cout << z << "\n";
+    for(auto i:ans)
+        cout << i << " ";
+}
+
+int main() 
+{
+    by_ANIKET
+    ll T = 1,t = 0;
+    // cin >> T;
+    while(t++ < T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
