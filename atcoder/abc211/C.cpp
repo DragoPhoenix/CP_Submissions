7acb8bac227afbc35e6f8ae1150d32f1a6b8d9da
Aniket Shukla <aniketshukla3553@gmail.com>
2021-07-24 18:04:00 +0530

    Add solution for problem `C - chokudai` Link: https://atcoder.jp/contests/abc211/tasks/abc211_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc211/submissions/24502608

---
+++ atcoder/abc211/C.cpp
@@ -0,0 +1,134 @@
+//Hey stalker :)
+#pragma GCC optimize("Ofast")
+#define _USE_MATH_DEFINES
+#include "bits/stdc++.h"
+using namespace std;
+
+#define max(a, b) (a < b ? b : a)
+#define min(a, b) (a > b ? b : a)
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define all(v)                      (v).begin(),(v).end()
+#define Sort(v)                     sort(all((v)));
+#define rSort(v)                    sort((v).rbegin(),(v).rend());
+#define tr(container, it) for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL);
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr4(a,b,c,d) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\t"<<#d<<"="<<d<<"\n";
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int india = 0; india<N;india++) cerr<<A[india]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int india=0;india<N;india++){cerr<<"[";for(int sachin=0;sachin<M;++sachin) cerr<<A[india][sachin]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int india = 0; india< (v).size(); india++) cerr<<v[india]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int india=0;india<v.size();india++){cerr<<"[";for(int sachin=0;sachin<(v[india].size());++sachin) cerr<<v[india][sachin]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto india = m.begin(); india!=m.end();india++) cerr<<*india<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto india = m.begin();india!=m.end(); india++) cerr<<"("<<india->first<<","<<india->second<<") ";cerr<<"]\n";
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N=2e5+5;
+
+void solve()
+{
+    ll n, m, t = 0, k = 0, x = 0, y = 0, z = 0, a1, a2, a3, a4, a5, var = 1, f = INF;
+    string s;
+
+    cin >> s;
+
+    n = s.length();
+    
+    vector<char> v = {'c', 'h', 'o', 'k', 'u', 'd', 'a', 'i'};
+
+    map <char, int> mp;
+    map <char, ll> bus;
+    vi a(n, 0);
+    FOR(i, 8)
+    bus[v[i]] = 0;
+    FORR(i, n-1, 0)
+    {
+        FOR(j, 8)
+        {
+            if(s[i] == v[j])
+            {
+                if(j == 7)
+                {
+                    mp[v[j]]++;
+                }
+                else if(j == 6)
+                {
+                    a[i] = mp[v[j+1]]; 
+                    mp[v[j]]++;
+                    bus[v[j]] = ( bus[v[j]] + a[i] ) % MOD;
+                }
+                else
+                {
+                    // a[i] = mp[v[j+1]];
+
+                    a[i] = bus[v[j+1]];
+                    mp[v[j]]++;
+                    bus[v[j]] = ( bus[v[j]] + a[i] ) % MOD;
+                }
+            }
+        }
+    }
+
+    // debugv(a)
+    
+    FOR(i, n)
+    {
+        if(s[i] == 'c')
+            z += a[i];
+    }
+    cout << z % MOD << "\n";
+}
+
+int main() 
+{
+    by_ANIKET
+    ll T = 1,t = 0;
+    // cin >> T;
+    while(t++ < T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
