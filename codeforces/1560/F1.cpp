ae76d2693316970fe8dd5843fb65daf62ff4db9f
Aniket Shukla <aniketshukla3553@gmail.com>
2021-09-13 15:53:00 +0530

    Add solution for problem `F1 - Nearest Beautiful Number (easy version)` Link: https://codeforces.com/contest/1560/problem/F1 Tags: binary search, bitmasks, brute force, constructive algorithms, dfs and similar, greedy, *1900 Ref: https://codeforces.com/contest/1560/submission/128715971

---
+++ codeforces/1560/F1.cpp
@@ -0,0 +1,156 @@
+//Hey stalker :)
+#include "bits/stdc++.h"
+using namespace std;
+
+#define MOD 998244353
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL);
+
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
+
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr4(a,b,c,d) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\t"<<#d<<"="<<d<<"\n";
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int india = 0; india<N;india++) cerr<<A[india]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int india=0;india<N;india++){cerr<<"[";for(int sachin=0;sachin<M;++sachin) cerr<<A[india][sachin]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int india = 0; india< (v).size(); india++) cerr<<v[india]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int india=0;india<v.size();india++){cerr<<"[";for(int sachin=0;sachin<(v[india].size());++sachin) cerr<<v[india][sachin]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto india = m.begin(); india!=m.end();india++) cerr<<*india<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto india = m.begin();india!=m.end(); india++) cerr<<"("<<india->first<<","<<india->second<<") ";cerr<<"]\n";
+
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N=2e5+5;
+
+set <ll> st;
+
+void make_all(ll c1, ll c2, ll l)
+{
+	string s, t;
+	char a = c1 + '0';
+	char b = c2 + '0';
+	
+	FOR(i, l)
+	s += '0';
+	t = s;
+
+	for(ll i = 0; i < (1 << l); i++)
+	{
+		s = t;
+		for(ll j = 0; j < l; j++)
+		{
+			if(i & (1 << j))
+				s[j] = a;
+			else
+				s[j] = b;
+		}
+		ll x = stoll(s);
+		st.insert(x);
+	}
+
+
+}
+
+void pre()
+{
+	FOR(i, 10)
+	{
+		FOR(j, 10)
+		{
+			FORL(l, 1, 12)
+			{
+				make_all(i, j, l);
+			}
+		}
+	}
+}
+
+void solve()
+{
+    ll n, m, t = 0, k = 0, x = 0, y = 0, z = 0, a1, a2, a3, a4, a5, var = 1, f = INF;
+    string s;
+
+    cin >> n >> k;
+
+    if(k == 1)
+    {
+    	s = to_string(n);
+    	
+    	if(s[0] == '9')
+    	{
+    		FOR(i, s.length())
+    		{
+    			cout << "9";
+    		}
+    		cout << "\n";
+    		return;
+    	}
+    	string a, b;
+    	FOR(i, s.length())
+    	{
+    		a += s[0];
+    		b += (s[0] + 1);
+    	}
+    	x = stoll(a);
+    	y = stoll(b);
+
+    	if(x >= n)
+    		cout << x << "\n";
+    	else
+    		cout << y << "\n";
+    	return;
+    }
+
+    if(st.find(10000000000) == st.end())
+    	assert(0);
+    cout << *st.lower_bound(n) << "\n";
+}
+
+int main() 
+{
+    by_ANIKET
+    ll T = 1,t = 0;
+    pre();
+    cin >> T;
+    while(t++ < T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓     
