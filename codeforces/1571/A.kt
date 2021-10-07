f316add5bce540066095959eaf5ed35dbd73f91d
Aniket Shukla <aniketshukla3553@gmail.com>
2021-10-07 20:14:00 +0530

    Add solution for problem `A - Sequence of Comparisons` Link: https://codeforces.com/contest/1571/problem/A Tags: *special problem Ref: https://codeforces.com/contest/1571/submission/131078069

---
+++ codeforces/1571/A.kt
@@ -0,0 +1,71 @@
+//Hey stalker :)
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.reflect.typeOf
+
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readLong() = readLn().toLong() // single long
+private fun readDouble() = readLn().toDouble() // single double
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readLongs() = readStrings().map { it.toLong() } // list of longs
+private fun readDoubles() = readStrings().map { it.toDouble() } // list of doubles
+
+fun solve()
+{
+    var s = readLn()
+
+    var n = s.length
+
+    var x = 0
+
+    for(i in 0..n-1)
+    {
+        if(s[i] == '<')
+        {
+            if(x > 1)
+            {
+                println("?")
+                return
+            }
+            x = 1
+        }
+        else if(s[i] == '>')
+        {
+            if(x == 1)
+            {
+                println("?")
+                return
+            }
+            x = 2
+        }
+
+    }
+    if(x == 1)
+        println("<")
+    else if(x == 2)
+        println(">")
+    else
+        println("=")
+}
+
+fun main()
+{
+    var t = 1
+    t = readInt()
+    for (q in 1..t) 
+    {
+        solve()
+    }
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//  
