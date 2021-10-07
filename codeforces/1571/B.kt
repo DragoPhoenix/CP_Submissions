f3549a17f23ab3ab32588d2c15f3fa9b8750eb04
Aniket Shukla <aniketshukla3553@gmail.com>
2021-10-07 20:22:00 +0530

    Add solution for problem `B - Epic Novel` Link: https://codeforces.com/contest/1571/problem/B Tags: *special problem, math Ref: https://codeforces.com/contest/1571/submission/131079066

---
+++ codeforces/1571/B.kt
@@ -0,0 +1,51 @@
+//Hey stalker :)
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.reflect.typeOf
+
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readLong() = readLn().toLong() // single long
+private fun readDouble() = readLn().toDouble() // single double
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readLongs() = readStrings().map { it.toLong() } // list of longs
+private fun readDoubles() = readStrings().map { it.toDouble() } // list of doubles
+
+fun solve()
+{
+    var n = readInt()
+    var (a, va) = readInts()
+    var (c, vc) = readInts()
+    var b = readInt()
+
+    var x = va
+    for(i in a+1..b)
+    {
+        x++
+    }
+    if(x < vc)
+        println("$x")
+    else if(x >= vc)
+        println("$vc")
+}
+
+fun main()
+{
+    var t = 1
+    t = readInt()
+    for (q in 1..t) 
+    {
+        solve()
+    }
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//  
