a5eeedb877e49ec785578830412f14b96cf4618d
Aniket Shukla <aniketshukla3553@gmail.com>
2021-10-11 00:56:00 +0530

    Add solution for problem `D - Flowers` Link: https://codeforces.com/contest/474/problem/D Tags: dp, *1700 Ref: https://codeforces.com/contest/474/submission/131495992

---
+++ codeforces/474/D.cpp
@@ -0,0 +1,89 @@
+#define _USE_MATH_DEFINES
+#include "bits/stdc++.h"
+using namespace std;
+
+#define max(a, b) (a < b? b : a)
+#define min(a, b) ((a>b)?b:a)
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define all(v)                      (v).begin(),(v).end()
+#define Sort(v)                     sort(all((v)))
+#define rSort(v)                    sort((v).rbegin(),(v).rend())
+#define tr(container, it) for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b)  (x/gcd(x,y))*y
+#define INF 9223372036854775807
+#define abs llabs
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define Clear( a, b ) memset( a, b, sizeof( a ) )
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int i = 0; i<N;i++) cerr<<A[i]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int i=0;i<N;i++){cerr<<"[";for(int j=0;j<M;++j) cerr<<A[i][j]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int i = 0; i< (v).size(); i++) cerr<<v[i]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int i=0;i<v.size();i++){cerr<<"[";for(int j=0;j<(v[i].size());++j) cerr<<v[i][j]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin(); itr!=m.end();itr++) cerr<<*itr<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin();itr!=m.end(); itr++) cerr<<"("<<itr->first<<","<<itr->second<<") ";cerr<<"]\n";
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N = 1e5+5;
+ll k;
+vi vp(N, 0);
+
+void pre()
+{
+    FORL(i, 1, k)
+    vp[i] = 1;
+    vp[k] = 2;
+    FORL(i, k+1, N-2)
+    {
+        vp[i] = ( vp[i-1] + vp[i-k] ) % MOD;
+    }
+    FORL(i, 1, N-2)
+    {
+        vp[i] = (vp[i] + vp[i-1]) % MOD;
+    }
+}
+
+void solve()
+{
+    ll n, m, t=0, kk=0, x=0, y=0, z=0, a1 = -1, a2, a3, a4, a5, var=1, f=INF;
+    cin >> x >> y;
+    cout << ( vp[y] - vp[x-1] + MOD ) % MOD << "\n";
+     
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll T = 1,t = 0;
+    cin >> T >> k;
+    pre();
+    while(t++ < T)
+        solve();
+}
