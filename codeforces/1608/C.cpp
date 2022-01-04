b387aeed3dc10e01b161271bebd93d26744aaa85
Aniket Shukla <aniketshukla3553@gmail.com>
2022-01-04 19:17:00 +0530

    Add solution for problem `C - Game Master` Link: https://codeforces.com/contest/1608/problem/C Tags: data structures, dfs and similar, dp, graphs, greedy, two pointers, *1700 Ref: https://codeforces.com/contest/1608/submission/141647290

---
+++ codeforces/1608/C.cpp
@@ -0,0 +1,143 @@
+//Hey stalker :)
+#include "bits/stdc++.h"
+using namespace std;
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL);
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr4(a,b,c,d) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\t"<<#d<<"="<<d<<"\n";
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int india = 0; india<N;india++) cerr<<A[india]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int india=0;india<N;india++){cerr<<"[";for(int sachin=0;sachin<M;++sachin) cerr<<A[india][sachin]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int india = 0; india< (v).size(); india++) cerr<<v[india]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int india=0;india<v.size();india++){cerr<<"[";for(int sachin=0;sachin<(v[india].size());++sachin) cerr<<v[india][sachin]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto india = m.begin(); india!=m.end();india++) cerr<<*india<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto india = m.begin();india!=m.end(); india++) cerr<<"("<<india->first<<","<<india->second<<") ";cerr<<"]\n";
+
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N=2e5+5;
+
+void solve()
+{
+    ll n, m, t = 0, k = 0, x = 0, y = 0, z = 0, a1, a2, a3, a4, a5, var = 1, f = INF;
+    string s;
+
+    cin >> n;
+    
+    vi a(n), b(n);
+    map<ll, ll> mp, mpb, mpab, mpba;
+    set<ll> sa, sb;
+    vector<pi> vp(n), rvp(n);
+    FOR(i, n)
+    {
+        cin >> a[i];
+        mp[a[i]] = i;
+        sa.insert(a[i]);
+    }
+    FOR(i, n)
+    {
+        cin >> b[i];
+        mpb[b[i]] = i;
+        vp[i] = {a[i], b[i]};
+        mpab[a[i]] = b[i];
+        mpba[b[i]] = a[i];
+        sb.insert(b[i]);
+    }
+
+    sort(vp.begin(), vp.end());
+
+    pi p1 = vp[n-1];
+    queue<pi> q;
+    q.push(p1);
+
+    sa.erase(p1.F);
+    sb.erase(p1.S);
+
+    FOR(i, n)
+    s += "0";
+
+    s[mp[p1.F]] = '1';
+    while(!q.empty())
+    {
+        p1 = q.front();
+        q.pop();
+        auto it = sa.upper_bound(p1.F);
+        while(it != sa.end())
+        {
+            x = *it;
+            it++;
+            y = mpab[x];
+            q.push({x, y});
+            sa.erase(x);
+            sb.erase(y);
+            s[mp[x]] = '1';
+        }
+
+        it = sb.upper_bound(p1.S);
+        while(it != sb.end())
+        {
+            y = *it;
+            it++;
+            x = mpba[y];
+            q.push({x, y});
+            sa.erase(x);
+            sb.erase(y);
+            s[mp[x]] = '1';
+        }
+    }
+
+    cout << s << "\n";
+}
+
+int main() 
+{
+    by_ANIKET
+    ll T = 1,t = 0;
+    cin >> T;
+    while(t++ < T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
