2d82bdb8f81a73b62a4a434de0fe7bba5469afa1
Aniket Shukla <aniketshukla3553@gmail.com>
2022-03-16 17:22:00 +0530

    Add solution for problem `E - Rescheduling the Exam` Link: https://codeforces.com/contest/1650/problem/E Tags: binary search, data structures, greedy, implementation, math, sortings, *1900 Ref: https://codeforces.com/contest/1650/submission/149811409

---
+++ codeforces/1650/E.cpp
@@ -0,0 +1,219 @@
+//Hey stalker :)
+#include "bits/stdc++.h"
+using namespace std;
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL);
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr4(a,b,c,d) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\t"<<#d<<"="<<d<<"\n";
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int india = 0; india<N;india++) cerr<<A[india]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int india=0;india<N;india++){cerr<<"[";for(int sachin=0;sachin<M;++sachin) cerr<<A[india][sachin]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int india = 0; india< (v).size(); india++) cerr<<v[india]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int india=0;india<v.size();india++){cerr<<"[";for(int sachin=0;sachin<(v[india].size());++sachin) cerr<<v[india][sachin]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto india = m.begin(); india!=m.end();india++) cerr<<*india<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto india = m.begin();india!=m.end(); india++) cerr<<"("<<india->first<<","<<india->second<<") ";cerr<<"]\n";
+
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N = 2e5+5;
+
+void solve()
+{
+    ll n, m, d, t = 0, k = 0, x = 0, y = 0, z = 0, a1, a2, a3, a4, a5, var = 1, f = INF;
+    string s;
+
+    cin >> n >> d;
+    
+    vi a(n + 1, 0);
+    FORL(i, 1, n + 1) cin >> a[i];
+
+    x = a[1] -  a[0] - 1;
+    FORL(i, 2, n + 1)
+        x = min(x, a[i] - a[i-1] - 1);
+
+    y = d + 1;
+
+    while(x < y)
+    {
+        m = x + (y - x + 1) / 2;
+        // pr3(x, y, m)
+        z = -1;
+        k = 1;
+        FORL(i, 1, n + 1)
+        {
+            if(a[i] - a[i-1] - 1 < m)
+            {
+                if(z != -1)
+                {
+                    k = 0;
+                    break;
+                }
+                else
+                {
+                    z = i;
+                    i++;
+                    if(i < n + 1 && a[i] - a[i-2] - 1 < m)
+                    {
+                        k = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        if(z == -1)
+        {
+            x = m;
+            continue;   
+        }
+        else if(!k)
+        {
+            y = m - 1;
+            continue;
+        }
+        else
+        {
+            k = 0;
+            a1 = a[0];
+            a2 = n;
+            if(z == n)
+                a2--;
+            
+            FORL(i, 1, n + 1)
+            {
+                if(i == z)
+                {
+                    continue;
+                }
+        
+                t = a[i] - a1 - 2;
+                t /= 2;
+                if(t >= m)
+                {
+                    k = 1;
+                    break;
+                }
+                a1 = a[i];
+            }
+            if(k || (d - a[a2] - 1) >= m)
+            {
+                x = m;
+                continue;
+            }
+            else
+            {
+                // pr1(m)
+                if(z == 1)
+                {
+                    y = m - 1;
+                    continue;
+                }
+                z--;
+                a1 = a[0];
+                k = 1;
+                FORL(i, 1, n + 1)
+                {
+                    if(i == z)
+                        continue;
+                    if(a[i] - a1 - 1 < m)
+                    {
+                        k = 0;
+                        break;
+                    }
+                    a1 = a[i];
+                }
+                if(!k)
+                {
+                    y = m - 1;
+                    continue;
+                }
+                
+                k = 0;
+                a1 = a[0];
+                a2 = n;
+                if(z == n)
+                    a2--;
+
+                FORL(i, 1, n + 1)
+                {
+                    if(i == z)
+                        continue;
+            
+                    t = a[i] - a1 - 2;
+                    t /= 2;
+                    if(t >= m)
+                    {
+                        // pr2(a[i], a1)
+                        k = 1;
+                        break;
+                    }
+                    a1 = a[i];
+                }
+                // pr3(k, d, a[a2])
+                if(k || (d - a[a2] - 1) >= m)
+                {
+                    x = m;
+                    continue;
+                }
+                else
+                {
+                    y = m - 1;
+                }
+            }
+        }
+    }
+
+    cout << x << "\n";
+}
+
+int main() 
+{
+    by_ANIKET
+    ll T = 1,t = 0;
+    cin >> T;
+    while(t++ < T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
