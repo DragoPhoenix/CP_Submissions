f611c66a4e91bd67ea1dba1dae23ba5e5ecd6f23
Aniket Shukla <aniketshukla3553@gmail.com>
2022-05-29 17:26:00 +0530

    Add solution for problem `B - Strange Definition` Link: https://codeforces.com/contest/1470/problem/B Tags: bitmasks, graphs, hashing, math, number theory, *1900 Ref: https://codeforces.com/contest/1470/submission/158798407

---
+++ codeforces/1470/B.cpp
@@ -0,0 +1,148 @@
+//Hey stalker :)
+#include "bits/stdc++.h"
+using namespace std;
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL);
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr4(a,b,c,d) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\t"<<#d<<"="<<d<<"\n";
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int india = 0; india<N;india++) cerr<<A[india]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int india=0;india<N;india++){cerr<<"[";for(int sachin=0;sachin<M;++sachin) cerr<<A[india][sachin]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int india = 0; india< (v).size(); india++) cerr<<v[india]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int india=0;india<v.size();india++){cerr<<"[";for(int sachin=0;sachin<(v[india].size());++sachin) cerr<<v[india][sachin]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto india = m.begin(); india!=m.end();india++) cerr<<*india<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto india = m.begin();india!=m.end(); india++) cerr<<"("<<india->first<<","<<india->second<<") ";cerr<<"]\n";
+
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N = 1e6+5;
+
+vi spf(N), rep(N);
+
+void pre()
+{
+    FORL(i, 1, N)
+        spf[i] = i;
+
+    for(int i = 2; i * i < N; i++)
+    {
+        if(spf[i] == i)
+        for(int j = i * i; j < N; j += i)
+            spf[j] = i;
+    }
+
+    ll x, y, z, r;
+
+    rep[1] = 1;
+    FORL(i, 2, N)
+    {
+        x = i;
+        r = 1;
+        while(x > 1)
+        {
+            y = spf[x];
+            z = 0;
+            while(x % y == 0)
+            {
+                x /= y;
+                z++;
+            }
+            if(z % 2)
+                r *= y;
+        }
+        rep[i] = r;
+    }
+}
+
+void solve()
+{
+    ll n, m, t = 0, k = 0, x = 0, y = 0, z = 0, a1, a2, a3, a4, a5, var = 1, f = INF;
+    string s;
+
+    cin >> n;
+    
+    vi a(n);
+    map<int, ll> mp;
+    FOR(i, n)
+    {
+        cin >> a[i];
+        a[i] = rep[a[i]];
+        mp[a[i]]++;
+    }
+
+    a1 = a2 = 0;
+    for(auto e:mp)
+    {
+        a1 = max(a1, e.S);
+        if(e.F == 1 || e.S % 2 == 0)
+        {
+            a2 += e.S;
+        }
+    }
+    a2 = max(a1, a2);
+
+    cin >> m;
+
+    FOR(i, m)
+    {
+        cin >> k;
+        if(!k)
+            cout << a1 << "\n";
+        else
+            cout << a2 << "\n";
+    }
+}
+
+int main() 
+{
+    by_ANIKET
+    ll T = 1,t = 0;
+    pre();
+    cin >> T;
+    while(t++ < T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
