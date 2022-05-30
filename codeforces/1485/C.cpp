e5b90f3982402d62bb64e93fea50ef8a8aefdbbb
Aniket Shukla <aniketshukla3553@gmail.com>
2022-05-30 17:56:00 +0530

    Add solution for problem `C - Floor and Mod` Link: https://codeforces.com/contest/1485/problem/C Tags: binary search, brute force, math, number theory, *1700 Ref: https://codeforces.com/contest/1485/submission/158871875

---
+++ codeforces/1485/C.cpp
@@ -0,0 +1,151 @@
+//Hey stalker :)
+#include "bits/stdc++.h"
+using namespace std;
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL);
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr4(a,b,c,d) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\t"<<#d<<"="<<d<<"\n";
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int india = 0; india<N;india++) cerr<<A[india]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int india=0;india<N;india++){cerr<<"[";for(int sachin=0;sachin<M;++sachin) cerr<<A[india][sachin]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int india = 0; india< (v).size(); india++) cerr<<v[india]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int india=0;india<v.size();india++){cerr<<"[";for(int sachin=0;sachin<(v[india].size());++sachin) cerr<<v[india][sachin]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto india = m.begin(); india!=m.end();india++) cerr<<*india<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto india = m.begin();india!=m.end(); india++) cerr<<"("<<india->first<<","<<india->second<<") ";cerr<<"]\n";
+
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N=2e5+5;
+
+void solve()
+{
+    ll n, m, t = 0, k = 0, x = 0, y = 0, z = 0, a1, a2, a3, a4, a5, var = 1, f = INF;
+    string s;
+
+    cin >> x >> y;
+    
+    if(x <= 2)
+    {
+        cout << "0\n";
+        return;
+    }
+
+    ll l = 2, r = 1e6;
+    while(l < r)
+    {
+        m = l + (r - l) / 2 + 1;
+        if((m - 1) * (m + 1) <= x)
+        {
+            l = m;
+        }
+        else
+        {
+            r = m - 1;
+        }
+    }
+    a1 = l;
+    a1 = min(a1, y);
+
+    z += a1 * (a1 - 1) / 2;
+
+    // pr2(z, a1)
+    a1++;
+    
+    FORL(i, 1, a1 + 1)
+    {
+        // z += x / i;
+        a2 = x / i;
+        a2 = min(a2, y + 1);
+        // pr1(z)
+        // pr3(x, i, a2)
+        if(a2 > a1)
+            z += a2 - a1;
+    }
+    cout << z << "\n";
+    /*
+    [a1 + 1, y + 1] -> how many numbers in this range * i <= x
+    a = b * x + y
+    x, y >= 0
+    for x = y:
+    a = b * x + x ; x < b
+    a = v * (b + 1) ; 1 <= v < b
+    => b + 1 is a factor of a
+
+    in Question :
+    1 ... x   1 ... y
+    2, 1
+    3, 2
+    x, x - 1  if x - 1 <= y
+
+    2, 1
+    3, 2
+    y + 1, y if y + 1 <= x
+
+    From y side:
+    For 1: divisible by 2 => all even numbers  -> INVALID as mod equals 0 always
+    For 2: div by 3 => x / 3
+    
+    For 2: b = 2 => * can be max 1 => b - 1 values
+    For 3: 2 values max: a = 4, 8
+    For 4: 3 values max: a = 5, 10, 15
+    
+    can take max values for all i such that (i - 1) * (i + 1) <= x
+    let this max value is a1 => z += a1 * (a1 - 1) / 2
+    
+    now for a1 + 1 onwards, we need count of multiples <= x
+    
+    */
+}
+
+int main() 
+{
+    by_ANIKET
+    ll T = 1,t = 0;
+    cin >> T;
+    while(t++ < T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
