86e47f56c9f2d39d3bb333d24bf442d4c048e93f
Aniket Shukla <aniketshukla3553@gmail.com>
2022-06-01 22:00:00 +0530

    Add solution for problem `B - Polycarp and the Language of Gods` Link: https://codeforces.com/contest/1431/problem/B Tags: *special problem, implementation, two pointers, *1400 Ref: https://codeforces.com/contest/1431/submission/159181331

---
+++ codeforces/1431/B.kt
@@ -0,0 +1,58 @@
+//Hey stalker :)
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.reflect.typeOf
+
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readLong() = readLn().toLong() // single long
+private fun readDouble() = readLn().toDouble() // single double
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readLongs() = readStrings().map { it.toLong() } // list of longs
+private fun readDoubles() = readStrings().map { it.toDouble() } // list of doubles
+
+fun solve()
+{
+    var s = readLn()
+    var z = 0
+    var n = s.length
+    var i = 0
+    while(i < n)
+    {
+        if(s[i] == 'w')
+            z++
+        else
+        {
+            var x = 0
+            while(i < n && s[i] == 'v')
+            {
+                i++
+                x++
+            }
+            i--
+            z += x / 2
+        }
+        i++
+    }
+    println("$z")
+}
+
+fun main()
+{
+    var t = readInt()
+    for (q in 1..t) 
+    {
+        solve()
+    }
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//  
