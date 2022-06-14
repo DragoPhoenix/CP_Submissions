8ab3c52010b83b3a4fd9108164d29479b6d2cfb3
Aniket Shukla <aniketshukla3553@gmail.com>
2022-06-14 20:50:00 +0530

    Add solution for problem `B - Nice Matrix` Link: https://codeforces.com/contest/1422/problem/B Tags: greedy, implementation, math, *1300 Ref: https://codeforces.com/contest/1422/submission/160585279

---
+++ codeforces/1422/B.cpp
@@ -0,0 +1,129 @@
+//Hey stalker :)
+#include "bits/stdc++.h"
+using namespace std;
+
+#define MOD 1000000007
+
+#define FOR(a,c)   for ( ll (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( ll (a)=(b); (a)<(c); (a)++)
+#define FORR(a,b,c)  for (  ll (a)=(b); (a)>=(c); (a)--)
+#define INF 9223372036854775807
+//18446744073709551615
+#define abs llabs
+#define by_ANIKET ios::sync_with_stdio(false);  cin.tie(NULL);
+
+#define no cout<<"No\n";
+#define yes cout<<"Yes\n";
+
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+#define pr4(a,b,c,d) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\t"<<#d<<"="<<d<<"\n";
+#define pr3(a,b,c) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\t"<<#c<<"="<<c<<"\n";
+#define pr2(a,b) cerr<<#a<<"="<<a<<"\t"<<#b<<"="<<b<<"\n";
+#define pr1(a) cerr<<#a<<"="<<a<<"\n";
+#define debug(x)       cerr<< #x <<" : "<< x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int india = 0; india<N;india++) cerr<<A[india]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int india=0;india<N;india++){cerr<<"[";for(int sachin=0;sachin<M;++sachin) cerr<<A[india][sachin]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int india = 0; india< (v).size(); india++) cerr<<v[india]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int india=0;india<v.size();india++){cerr<<"[";for(int sachin=0;sachin<(v[india].size());++sachin) cerr<<v[india][sachin]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto india = m.begin(); india!=m.end();india++) cerr<<*india<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto india = m.begin();india!=m.end(); india++) cerr<<"("<<india->first<<","<<india->second<<") ";cerr<<"]\n";
+
+typedef unsigned long long ull;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+
+inline long long  max3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  min3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+
+const ll N=2e5+5;
+
+void solve()
+{
+    ll n, m, t = 0, k = 0, x = 0, y = 0, z = 0, a1, a2, a3, a4, a5, var = 1, f = INF;
+    string s;
+
+    cin >> n >> m;
+    
+    vector<vi> a(n, vi(m));
+
+    FOR(i, n)
+        FOR(j, m)
+            cin >> a[i][j];
+
+    FOR(i, n / 2)
+    {
+        FOR(j, m / 2)
+        {
+            vector<int> v;
+            v.PB(a[i][j]);
+            v.PB(a[n-1-i][j]);
+            v.PB(a[i][m-1-j]);
+            v.PB(a[n-1-i][m-1-j]);
+            
+            set<int> st(v.begin(), v.end());
+            x = INF;
+            FOR(p, 4)
+            {
+                y = 0;
+                FOR(q, 4)
+                {
+                    y += abs(v[p]-v[q]);
+                }
+                x = min(x, y);
+            }
+            z += x;
+        }
+    }
+
+    if(n % 2)
+    {
+        FOR(j, m / 2)
+        {
+            z += abs(a[n/2][j] - a[n/2][m-1-j]);
+        }
+    }
+
+    if(m % 2)
+    {
+        FOR(i, n / 2)
+        {
+            z += abs(a[i][m/2] - a[n-1-i][m/2]);
+        }
+    }
+
+    cout << z << "\n";
+
+}
+
+int main() 
+{
+    by_ANIKET
+    ll T = 1,t = 0;
+    cin >> T;
+    while(t++ < T)
+    {
+        // cout<<"Case #"<<t<<":"<<' ';
+        solve();
+        // cout<<'\n';
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+//   ©
+//                      ______  __    __ ______ __    __ ________ ________ 
+//                     /      \|  \  |  \      \  \  /  \        \        \
+//                    |  ▓▓▓▓▓▓\ ▓▓\ | ▓▓\▓▓▓▓▓▓ ▓▓ /  ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+//  ______ ______     | ▓▓__| ▓▓ ▓▓▓\| ▓▓ | ▓▓ | ▓▓/  ▓▓| ▓▓__      | ▓▓   
+// |      \      \    | ▓▓    ▓▓ ▓▓▓▓\ ▓▓ | ▓▓ | ▓▓  ▓▓ | ▓▓  \     | ▓▓   
+//  \▓▓▓▓▓▓\▓▓▓▓▓▓    | ▓▓▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ | ▓▓ | ▓▓▓▓▓\ | ▓▓▓▓▓     | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓ \▓▓▓▓_| ▓▓_| ▓▓ \▓▓\| ▓▓_____   | ▓▓   
+//                    | ▓▓  | ▓▓ ▓▓  \▓▓▓   ▓▓ \ ▓▓  \▓▓\ ▓▓     \  | ▓▓   
+//                     \▓▓   \▓▓\▓▓   \▓▓\▓▓▓▓▓▓\▓▓   \▓▓\▓▓▓▓▓▓▓▓   \▓▓   
